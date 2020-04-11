9228971987d51eb33e5143a2fd3e779d2ad7ca62
Kelyn Paul Njeri <kelynpaul20.kp@gmail.com>
2020-04-11 23:35:45 +0300

    Calculate infectionsByRequestedTime.

--- src/estimator.py
+++ src/estimator.py
@@ -1,17 +1,28 @@
+def current_number_of_infections(period, period_type):
+  factor = 0
+  if period_type == 'months':
+    month_to_days = int(period) * 30
+    factor = month_to_days // 3
+  elif period_type == 'weeks':
+    weeks_to_days = int(period) * 7
+    factor = weeks_to_days // 3
+  else:
+    factor = int(period) // 3
+  return factor
+
 def estimator(data):
-  reported_cases = 0
-  for key, value in data.items():
-    if key == 'reportedCases':
-        reported_cases = value
-  
+  factor = current_number_of_infections(data['timeToElapse'], data['periodType']) 
   output_data = {
     'data': data,
     'estimated': {
       'impact': {
-        'currentlyInfected': reported_cases * 10
+        'currentlyInfected': data['reportedCases'] * 10,
+        'infectionsByRequestedTime': (data['reportedCases'] * 10) * (2 ** factor)
       },
       'severeImpact': {
-        'currentlyInfected': reported_cases * 50
+        'currentlyInfected': data['reportedCases'] * 50,
+        'infectionsByRequestedTime': (data['reportedCases'] * 50) * (2 ** factor)
+        
       }
     } 
   }
