a83a9ef628c11e29a599a1b285dabd1c51495542
Kelyn Paul Njeri <kelynpaul20.kp@gmail.com>
2020-04-11 22:40:37 +0300

    Compute value of currentlyInfected

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,16 @@
 def estimator(data):
-  return data
+  reported_cases = 0
+  for key, value in data.items():
+    if key is 'reportedCases':
+        reported_cases = value
+  
+  output_data = {
+    'data': data,
+    'impact': {
+      'currentlyInfected': reported_cases * 10
+    },
+    'severeImpact': {
+      'currentlyInfected': reported_cases * 50
+    }
+  }
+  return output_data
