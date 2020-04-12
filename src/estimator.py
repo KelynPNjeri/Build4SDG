90f1d8035b8dfcd4b5a58dc9194c6f80253e93fc
Kelyn Paul Njeri <kelynpaul20.kp@gmail.com>
2020-04-12 23:46:05 +0300

    Add solution for challenge 2 and challenge 3

--- src/estimator.py
+++ src/estimator.py
@@ -21,7 +21,7 @@ def estimator(data):
       'hospitalBedsByRequestedTime': int((data['totalHospitalBeds'] * .35) - (.15 * (data['reportedCases'] * 10) * (2 ** factor))),
       'casesForICUByRequestedTime': int(.05 * ((data['reportedCases'] * 10) * (2 ** factor))),
       'casesForVentilatorsByRequestedTime': int(0.02 * ((data['reportedCases'] * 10) * (2 ** factor))),
-      'dollarsInFlight': int(((data['reportedCases'] * 10) * (2 ** factor) * data['region']['avgDailyIncomePopulation'] * data['region']['avgDailyIncomeInUSD']) / 30)
+      'dollarsInFlight': int((((data['reportedCases'] * 10) * (2 ** factor)) * data['region']['avgDailyIncomePopulation'] * data['region']['avgDailyIncomeInUSD']) / 30)
     },
     'severeImpact': {
       'currentlyInfected': data['reportedCases'] * 50,
@@ -30,7 +30,23 @@ def estimator(data):
       'hospitalBedsByRequestedTime': int((data['totalHospitalBeds'] * .35) - (.15 * (data['reportedCases'] * 50) * (2 ** factor))),
       'casesForICUByRequestedTime': int(.05 * ((data['reportedCases'] * 50) * (2 ** factor))),
       'casesForVentilatorsByRequestedTime': int(.02 * ((data['reportedCases'] * 50) * (2 ** factor))),
-      'dollarsInFlight': int(((data['reportedCases'] * 50) * (2 ** factor) * data['region']['avgDailyIncomePopulation'] * data['region']['avgDailyIncomeInUSD']) / 30)
+      'dollarsInFlight': int((((data['reportedCases'] * 50) * (2 ** factor)) * data['region']['avgDailyIncomePopulation'] * data['region']['avgDailyIncomeInUSD']) / 30)
     }
   } 
   return output_data
+
+# if __name__ == "__main__":
+#     data = {
+#       'region': {
+#         'name': "Africa",
+#         'avgAge': 19.7,
+#         'avgDailyIncomeInUSD': 5,
+#         'avgDailyIncomePopulation': 0.71
+#       },
+#       'periodType': "days",
+#       'timeToElapse': 58,
+#       'reportedCases': 674,
+#       'population': 66622705,
+#       'totalHospitalBeds': 1380614
+# }
+# print(estimator(data))
